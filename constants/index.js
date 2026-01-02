03f85ae351168db02a048beb4545f63011a72c8a
Hariom Kashyap <omkashyap6870@gmail.com>
2026-01-02 14:23:41 +0530

    adding constants variables

---
+++ constants/index.js
@@ -0,0 +1,28 @@
+export const gitActions = [
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+  "heading",
+];
+
+// navigational links
+export const navLinks = [
+  {
+    link: "home",
+    uri: "#",
+  },
+  {
+    link: "editor",
+    uri: "#",
+  },
+];
